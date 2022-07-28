b6df548bb15003ea1214af9e09f4ba457180e733
Chamara-Wijepala <mr.worf777@gmail.com>
2022-07-28 15:39:46 +0530

    Add lazy loading functionality to each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/home";
-import Destination from "./pages/destination";
-import Crew from "./pages/crew";
-import Technology from "./pages/technology";
+const Home = lazy(() => import("./pages/home"));
+const Destination = lazy(() => import("./pages/destination"));
+const Crew = lazy(() => import("./pages/crew"));
+const Technology = lazy(() => import("./pages/technology"));
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Home />} />
-      <Route path="/destination/:id" element={<Destination />} />
-      <Route path="/crew/:id" element={<Crew />} />
-      <Route path="/technology/:id" element={<Technology />} />
+      <Route
+        path=""
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/destination/:id"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Destination />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/crew/:id"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Crew />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/technology/:id"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Technology />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
