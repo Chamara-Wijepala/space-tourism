f3dbe3b707f8e8cb656062ca453813e511c612f8
Chamara-Wijepala <mr.worf777@gmail.com>
2022-07-30 19:50:00 +0530

    Add CustomButton styled component

---
+++ src/styles/CustomButton.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const CustomButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+`;
+
+export default CustomButton;
