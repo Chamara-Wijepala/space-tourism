d6d150531ab3e9333bee372c9a3dce2bc17d8704
Chamara-Wijepala <mr.worf777@gmail.com>
2022-08-03 21:38:54 +0530

    Add styling to nav links

--- src/components/styles/Header.styled.ts
+++ src/components/styles/Header.styled.ts
@@ -2,4 +2,15 @@ import styled from "styled-components";
 
 export const Header = styled.header``;
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  a {
+    outline: none;
+    text-decoration: none;
+    letter-spacing: var(--spacing-2);
+    color: inherit;
+  }
+
+  a span {
+    font-weight: bold;
+  }
+`;
