c01bcacb51b537d18bcd3a30324adf5e23b99d10
Chamara-Wijepala <mr.worf777@gmail.com>
2022-08-03 22:12:10 +0530

    Add frosted glass effect to nav

--- src/components/styles/Header.styled.ts
+++ src/components/styles/Header.styled.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const Header = styled.header``;
 
 export const Nav = styled.nav`
+  /* Frosted glass effect */
+  background-color: rgba(var(--clr-white) / 0.04);
+  backdrop-filter: blur(1.5rem);
+
   a {
     outline: none;
     text-decoration: none;
